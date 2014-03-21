9302f3dc1123fc178dc50bdd5b52a5fee950a92d
Ascariel <pablocangas@gmail.com>
2014-03-21 18:29:03 -0400

    First Commit

---
+++ index.php
@@ -0,0 +1,33 @@
+
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+		<title>"Zolvitek: Construction"</title>
+
+		<STYLE TYPE="text/css" >
+		body {
+
+
+			background: url("http://www.techmynd.com/wp-content/uploads/2009/09/under-construction-20-19.jpg");
+			background-size: cover;
+			color: white;
+			text-align:center;
+			background-repeat:no-repeat;
+			background-attachment:fixed;
+			background-position: center center;
+			}
+		</STYLE>
+	</head>
+	<body>
+
+	<h1>Zolvitek: Solving Technologies</h1>
+
+
+
+
+
+
+	</body>
+</html>
---
+++ zolvitek.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+ <html>
+    <head>
+
+    <link type="text/css" rel="stylesheet" href="stylesheet.css" />
+    <title>ZOLVITEK</title>
+
+    </head>
+
+
+    <body>
+
+
+
+    	<div id="header">
+
+   			 <div id="title_logo">
+
+    	     <img id="zlogo" src="/i/logo.jpg" />
+
+   			 <div id="title_zolvitek">ZOLVITEK: </div>
+			 <div id="flavor_text">Solving Technologies</div>
+			</div>
+		<div/>
+
+
+
+
+   		<div id="left_panel">
+
+			<div id=left_MenusGroup>
+   				<div class="left_buttons"><p>Home</p></div>
+ 				<div class="left_buttons"><p></p>Services /Skills </div>
+				<div class="left_buttons"><p>Portfolio</p></div>
+    			<div class="left_buttons"><p>Client Feedback</p></div>
+    			<div class="left_buttons"><p>Contact Info</p></div>
+    			<div class="left_buttons"><p>F.A.Q</p></div>
+			</div>
+
+
+		</div>
+
+
+
+   		<div id="right_panel">
+
+		<div id="right_head" class="right_buttons">Related Links</div>
+ 		<div class="right_buttons">Develop Coding Skills </div>
+		<div class="right_buttons">Code Academy</div>
+ 	    <div class="right_buttons">Mozilla Developers Network</div>
+ 	    <div class="right_buttons">W3Schools</div>
+	    <div class="right_buttons">ZOLVITEK Update Log</div>
+
+
+		</div>
+
+
+ 		<div id="footer">
+
+		<div>Something Something</div>
+
+		</div>
+
+
+
+
+
+    </body>
+
+ </html>
